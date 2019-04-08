708c6ab4e46846d7e377080212d9f55405f43183
dmiryan <49276202+dmiryan@users.noreply.github.com>
2019-04-08 17:53:00 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-variable "access_key" {default = "AKIAYDJHKWD4R6FNKW4O"}
-variable "secret_key" {default = "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"}
+variable "access_key" {default = ""}
+variable "secret_key" {default = ""}
 variable "region"     {default = "us-east-1"}
 variable "key_name"   {default = "webservers"}
-variable "key_path"   {default = "/mnt/d/amazon/webservers.pem"}
+variable "key_path"   {default = ""}
 
 /*variable "amis" {
   type = "map"
